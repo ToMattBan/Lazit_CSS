843d7accb82435a5b15bd7d7b947740186b866ee
ToMattBan <souaquelecleyton@yandex.com>
2023-07-11 18:06:28 -0400

    tryin build function

---
+++ build.cjs
@@ -0,0 +1,57 @@
+var fs = require("fs");
+
+var lazitConfigs = {};
+
+var throwError = function (error) {
+  console.error(error);
+  throw error;
+};
+
+var readJsons = function () {
+  var defaultJson = {};
+  fs.readFile("./lazit_default.conf.json", "utf8", function (err, data) {
+    if (err) return throwError("Couldn't read the default json file");
+    if (!data) return throwError("Couldn't get the default JSON");
+
+    defaultJson = JSON.parse(data);
+    return;
+  });
+
+  fs.readFile("./lazit.conf.json", "utf8", function (err, data) {
+    if (err) throwError("Couldn't read the config json file");
+    if (!data) throwError("Couldn't get the config JSON");
+
+    data = JSON.parse(data);
+    lazitConfigs = data;
+
+    if (!data.settings) {
+      lazitConfigs.settings = defaultJson.settings;
+      console.log("a", data.settings);
+    }
+
+    console.log(lazitConfigs);
+    return;
+  });
+};
+
+readJsons();
+
+/* const init = async () => {
+  await readJsons();
+  console.log(lazitConfigs);
+};
+
+init(); */
+
+/* const input = `
+$mq-breakpoints: (
+  "tabletV": 600px,
+  "tabletH": 900px,
+  "laptop": 1200px,
+  "desktop": 1600px,
+) !default;
+`;
+
+fs.writeFile("./sass/1_settings/_breakpoints.scss", input, (err) => {
+  if (err) console.log(err);
+}); */
