949ee8b0b6653a8af8747e77f8596a0878161660
ToMattBan <cleyton_laz@protonmail.com>
2023-08-14 16:30:09 -0300

    Building settings

--- build/build.cjs
+++ build/build.cjs
@@ -1,6 +1,8 @@
 var fs = require("fs");
 
 var lazitConfigs = {};
+const projectBasePath = process.cwd();
+const baseSassPath = projectBasePath + "/sass";
 
 const throwError = function (error) {
   console.error(error);
@@ -11,11 +13,23 @@ const writeFile = function (content, filePath) {
   fs.writeFile(filePath, content, (err) => {
     if (err) throwError(err);
   })
+};
+
+const mergeAndDiscardDuplicates = function (json1, json2) {
+  const mergedJson = { ...json2 };
+
+  for (const key in json1) {
+    if (!mergedJson.hasOwnProperty(key)) {
+      mergedJson[key] = json1[key];
+    }
+  }
+
+  return mergedJson;
 }
 
 const readJsons = function () {
   let defaultJson = {};
-  fs.readFile("./lazit_default.conf.json", "utf8", function (err, data) {
+  fs.readFile(`${projectBasePath}/build/lazit_default.conf.json`, "utf8", function (err, data) {
     if (err) return throwError("Couldn't get the default json file");
     if (!data) return throwError("Couldn't read the default JSON");
 
@@ -23,7 +37,7 @@ const readJsons = function () {
     return;
   });
 
-  fs.readFile("./lazit.conf.json", "utf8", function (err, data) {
+  fs.readFile(`${projectBasePath}/lazit.conf.json`, "utf8", function (err, data) {
     if (err) throwError("Couldn't get the config json file");
     if (!data) throwError("Couldn't read the config JSON");
 
@@ -31,7 +45,7 @@ const readJsons = function () {
     lazitConfigs = data;
 
     for (key in defaultJson) {
-      if(!data[key]) {
+      if (!data[key]) {
         lazitConfigs[key] = defaultJson[key];
       }
     }
@@ -47,11 +61,92 @@ const buildCore = function (setupConfigs) {
     `\n$defaultFontSize: ${setupConfigs.defaultFontSize};` +
     '\n$utilities: ();';
 
-  writeFile(content, "./sass/1_settings/_core.scss");
-}
+  writeFile(content, `${baseSassPath}/1_settings/_core.scss`);
+};
+
+const buildSettings = async function (settingsConfigs) {
+  const settingsEnabled = ['core'];
+
+  if (settingsConfigs.breakpoints.enabled) {
+    settingsEnabled.push('breakpoints')
+    const breakpointConfigs = settingsConfigs.breakpoints;
+    let content = '$mq-breakpoints: (';
+
+    for (breakpoint in breakpointConfigs.breakpoints) {
+      content += `\n"${breakpoint}": ${breakpointConfigs.breakpoints[breakpoint]},`;
+    }
+
+    content += '\n);' +
+      `\n$breakpointSeparator: \\${breakpointConfigs.separator};`
+
+    writeFile(content, `${baseSassPath}/1_settings/_breakpoints.scss`);
+  }
+
+  if (settingsConfigs.colors.enabled) {
+    settingsEnabled.push('colors')
+    const colorsConfigs = settingsConfigs.colors;
+    let colorsJson = colorsConfigs.colors;
+
+    if (colorsConfigs.includeHTMLColors) {
+      let defaultColors = await fs.readFileSync(`${projectBasePath}/build/html-defaullt-colors.json`, "utf8", function (err, data) {
+        if (err) return throwError("Couldn't get the default colors JSON");
+        if (!data) return throwError("Couldn't read the default colors JSON");
+
+        defaultColors = JSON.parse(data);
+        return defaultColors;
+      });
+
+      defaultColors = JSON.parse(defaultColors);
+      colorsJson = mergeAndDiscardDuplicates(defaultColors, colorsConfigs.colors);
+    }
+
+    let content = '$colors: (';
+    for (color in colorsJson) {
+      content += `\n"${color}": ${colorsJson[color]},`;
+    }
+    content += '\n);' +
+      '\n@function getColor($key) {' +
+      '\n  @return map.get($colors, $key);' +
+      '\n}';
+
+    for (mainColor in colorsConfigs.projectMainColors) {
+      content += `\n$colors: map.set($colors, "${mainColor}", getColor("${colorsConfigs.projectMainColors[mainColor].sameAs}"));`
+    }
+
+    writeFile(content, `${baseSassPath}/1_settings/_colors.scss`);
+  }
+
+  if (settingsConfigs.spacing.enabled) {
+    settingsEnabled.push('spacing')
+    const spacingConfigs = settingsConfigs.spacing;
+
+    let content = `$defaultSpacingValue: ${spacingConfigs.defaultSpacingValue};` +
+      '\n$spacingFactors: (';
+
+    for (spacing in spacingConfigs.spacements) {
+      content += `\n"${spacing}": ${spacingConfigs.spacements[spacing]},`
+    }
+
+    content += '\n);' +
+      '\n$spacing: ();' +
+      '\n@each $spaceName, $spaceFactor in $spacingFactors {' +
+      ' \n$spacing: map.set($spacing, $spaceName, $defaultSpacingValue * $spaceFactor);' +
+      '\n}';
+
+    writeFile(content, `${baseSassPath}/1_settings/_spacing.scss`);
+  }
+
+  let content = "";
+  for (settings of settingsEnabled) {
+    content += `\n@import './${settings}';`
+  }
+
+  writeFile(content, `${baseSassPath}/1_settings/_mainSettings.scss`)
+};
 
 const buildLib = function (lazitConfigs) {
   buildCore(lazitConfigs.setup);
-}
+  buildSettings(lazitConfigs.settings);
+};
 
 readJsons();
