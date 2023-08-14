d2d24c499773d5b7c482fcb9ea3281083a857ef9
ToMattBan <cleyton_laz@protonmail.com>
2023-08-14 12:24:22 -0300

    function to build core file

--- build/build.cjs
+++ build/build.cjs
@@ -2,13 +2,19 @@ var fs = require("fs");
 
 var lazitConfigs = {};
 
-var throwError = function (error) {
+const throwError = function (error) {
   console.error(error);
   throw error;
 };
 
-var readJsons = function () {
-  var defaultJson = {};
+const writeFile = function (content, filePath) {
+  fs.writeFile(filePath, content, (err) => {
+    if (err) throwError(err);
+  })
+}
+
+const readJsons = function () {
+  let defaultJson = {};
   fs.readFile("./lazit_default.conf.json", "utf8", function (err, data) {
     if (err) return throwError("Couldn't get the default json file");
     if (!data) return throwError("Couldn't read the default JSON");
@@ -17,46 +23,35 @@ var readJsons = function () {
     return;
   });
 
-  fs.readFile("../lazit.conf.json", "utf8", function (err, data) {
+  fs.readFile("./lazit.conf.json", "utf8", function (err, data) {
     if (err) throwError("Couldn't get the config json file");
     if (!data) throwError("Couldn't read the config JSON");
 
     data = JSON.parse(data);
     lazitConfigs = data;
 
-    if (!data.setup) {
-      lazitConfigs.setup = defaultJson.setup;
-      console.log('setup', data.setup);
+    for (key in defaultJson) {
+      if(!data[key]) {
+        lazitConfigs[key] = defaultJson[key];
+      }
     }
 
-    if (!data.settings) {
-      lazitConfigs.settings = defaultJson.settings;
-      console.log("settings", data.settings);
-    }
-
-    console.log(lazitConfigs);
+    buildLib(lazitConfigs);
     return;
   });
 };
 
+const buildCore = function (setupConfigs) {
+  const content = `$utilityPrefix: "${setupConfigs.prefix}";` +
+    `\n$utilitySeparator: "${setupConfigs.separator}";` +
+    `\n$defaultFontSize: ${setupConfigs.defaultFontSize};` +
+    '\n$utilities: ();';
+
+  writeFile(content, "./sass/1_settings/_core.scss");
+}
+
+const buildLib = function (lazitConfigs) {
+  buildCore(lazitConfigs.setup);
+}
+
 readJsons();
-
-/* const init = async () => {
-  await readJsons();
-  console.log(lazitConfigs);
-};
-
-init(); */
-
-/* const input = `
-$mq-breakpoints: (
-  "tabletV": 600px,
-  "tabletH": 900px,
-  "laptop": 1200px,
-  "desktop": 1600px,
-);
-`;
-
-fs.writeFile("./sass/1_settings/_breakpoints.scss", input, (err) => {
-  if (err) console.log(err);
-}); */
