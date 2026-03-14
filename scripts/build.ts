2fa978f3bc5c3425b0935c59b9d714dbfb952415
ToMattBan <cleyton_laz@protonmail.com>
2026-03-14 09:54:26 -0300

    Default config done

---
+++ scripts/build.ts
@@ -0,0 +1,9 @@
+import { IConfig } from "../types/interfaces";
+
+function build(config: IConfig) {
+  console.log('building with', config)
+
+  // margin, padding always add auto
+}
+
+export default build
--- types/interfaces.ts
+++ types/interfaces.ts
@@ -1,31 +1,42 @@
 import * as CSS from "csstype"
 
-type IDirections = 'top' | 'bottom' | 'left' | 'right'
+type IDirections = 'top' | 'bottom' | 'left' | 'right';
 
-type ValidCSSKeys<K extends keyof CSS.Properties> = CSS.Properties[K] extends string | undefined ? CSS.Properties[K] : never
-type CSSKeysForRecord<K extends keyof CSS.Properties> = Exclude<ValidCSSKeys<K>, undefined>
-interface IUtility<K extends keyof CSS.Properties> {
-  shorthand: string
-  values: Partial<Record<CSSKeysForRecord<K>, string>>
+type ValidCSSKeys<K extends keyof CSS.Properties> = CSS.Properties[K] extends string | undefined ? CSS.Properties[K] : never;
+type CSSKeysForRecord<K extends keyof CSS.Properties> = Exclude<ValidCSSKeys<K>, undefined>;
+type IUtility<K extends keyof CSS.Properties> = {
+  shorthand: string;
+  type?: 'value';
+  values: Partial<Record<CSSKeysForRecord<K>, string>>;
+} | {
+  shorthand: string;
+  type: 'directional' | 'incrementalSize';
 }
+
 export interface IConfig {
-  prefix?: string,
+  prefix?: string;
   responsive?: {
     breakpoints: {
-      [key: string]: number
+      [key: string]: number;
     },
-    divisor: string,
+    divisor: string;
   },
+  grid: number;
   directions?: {
-    [key: string]: IDirections | IDirections[]
+    [key: string]: IDirections | IDirections[];
   }
   sizes?: {
-    [key: string]: string
+    [key: string]: string;
   },
   colors?: {
-    [key: string]: string
+    addBackgroundColor: boolean;
+    addTextColor: boolean;
+    addBorderColor: boolean;
+    colors: {
+      [key: string]: string;
+    }
   },
   utilities?: {
-    [K in keyof CSS.Properties]?: IUtility<K>
+    [K in keyof CSS.Properties]?: IUtility<K>;
   }
 }
